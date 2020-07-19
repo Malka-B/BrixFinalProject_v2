52f1d3985d5b5d5142d716bcf359e2eae89761f3
Blumy Kind <blumy0435@gmail.com>
2020-07-19 12:35:07 +0300

    adding controllers

---
+++ Account.Api/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Account.WebApi.Controllers
+{
+    public class LoginController : ControllerBase
+    {
+        
+    }
+}
--- Account.Service/Models/AccountModel.cs
+++ Account.Service/Models/AccountModel.cs
@@ -6,6 +6,8 @@ namespace Account.Service.Models
 {
     public class AccountModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateTime OpenDate { get; set; }
         public float Balance { get; set; }
     }
